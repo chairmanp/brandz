e189f35dc7c4b3e5b62c72fa15d9df74d930ce98
Ron Park <chairmanp@arkserver>
2012-12-15 20:04:58 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandz::Application.config.secret_token = '67fb38bff94f54616474c1aa1be6714e707475dc304c8fbe10355d7baaac83f67269212194879f38d997c0bcedf520547e1f5660fbe3d6bc18e54b94eeb9acff'
