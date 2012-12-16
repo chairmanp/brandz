94a608f7e7a8b7ca6bfcccb543029703a982782b
chairmanp <chairmanp@arkserver>
2012-12-16 15:07:18 -0800

    association added between brand and promotion

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,5 @@
 class Brand < ActiveRecord::Base
   attr_accessible :description, :logo, :name
+  has_many :promotions
+  validates :name, :length => { :maximum => 25 } 
 end
--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -1,3 +1,4 @@
 class Promotion < ActiveRecord::Base
   attr_accessible :defaultPrice, :description, :highPrice, :image, :lowPrice, :name
+  belongs_to :brand
 end
